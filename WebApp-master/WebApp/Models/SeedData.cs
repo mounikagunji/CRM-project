f4b55812669a88187090560b5592cc81620bb7fd
Mounika Gunji <gunjimounika14@gmail.com>
2024-06-12 14:06:39 +0530

    Add files via upload

---
+++ WebApp-master/WebApp/Models/SeedData.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebApp.Data;
+using System;
+using System.Linq;
+
+
+namespace WebApp.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new WebAppContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<WebAppContext>>()))
+            {
+                // Look for any movies.
+                if (context.Users.Any())
+                {
+                    return;   // DB has been seeded
+                }
+                context.Users.AddRange(
+                    new Users
+                    {
+                        Name = "Adam",
+                        Surname = "Nowak",
+                        DateOfBirth = DateTime.Parse("1984-04-25"),
+                        login = "adamnowak@crm.com"        
+                    },
+                    new Users
+                    {
+                        Name = "Michał",
+                        Surname = "Lewandowski",
+                        DateOfBirth = DateTime.Parse("1989-06-14"),
+                        login = "michallewandowski@crm.com"
+                    },
+                    new Users
+                    {
+                        Name = "Jerzy",
+                        Surname = "Czerniak",
+                        DateOfBirth = DateTime.Parse("1990-11-30"),
+                        login = "jerzyczerniak@crm.com"
+                    },
+                    new Users
+                    {
+                        Name = "Adrian",
+                        Surname = "Kowalski",
+                        DateOfBirth = DateTime.Parse("1979-01-01"),
+                        login = "adriankowalski@crm.com"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ WebApp-master/WebApp/Models/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp.Models
+
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        [Display(Name = "Date of birth")]
+        public DateTime DateOfBirth { get; set; }
+        public string login { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
